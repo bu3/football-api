c39faf526169706ee82011a0a3ddf9b8154275c5
Fabio Mangione <fabio.mangione@gmail.com>
2016-04-17 15:52:58 +0100

    Added CORS filter

--- src/main/groovy/com/football/data/Application.groovy
+++ src/main/groovy/com/football/data/Application.groovy
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
@@ -16,5 +18,17 @@ class Application extends WebMvcConfigurerAdapter {
     @Bean
     RestTemplate restTemplate() {
         new RestTemplate()
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping('/**')
+                        .allowedMethods('GET', 'POST', 'PUT', 'DELETE')
+                        .allowedOrigins('*')
+            }
+        };
     }
 }
